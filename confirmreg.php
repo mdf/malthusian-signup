391ac078fbafcfb612aab35f697069f797600172
Adam Sporne <eskpe@Adam-Spornes-MacBook-Pro.local>
2012-10-12 17:06:18 +0100

    Looks good but getting lots of errors - Cannot send session cache limiter - headers already sent ??

--- confirmreg.php
+++ confirmreg.php
@@ -1,3 +1,101 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<!-- Title -->
+<title>We Are AMBER</title>
+
+<!-- Start >> Meta Tags and Inline Scripts -->
+<meta name='robots' content='noindex,nofollow' />
+<link rel="alternate" type="application/rss+xml" title="We Are AMBER &raquo; Feed" href="http://www.weareamber.com/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="We Are AMBER &raquo; Comments Feed" href="http://www.weareamber.com/?feed=comments-rss2" />
+
+<!-- Styles -->
+<link rel='stylesheet' id='pagelines-less-css'  href='http://www.weareamber.com/?pageless=1_1349824153' type='text/css' media='all' />
+<link rel='stylesheet' id='pagelines-child-stylesheet-css'  href='http://www.weareamber.com/wp-content/themes/spitzer/style.css?ver=232-100292712' type='text/css' media='all' />
+
+<!-- Scripts -->
+<script type='text/javascript' src='http://www.weareamber.com/wp-includes/js/jquery/jquery.js?ver=1.7.2'></script>
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/sections/navbar/navbar.js?ver=3.4.2'></script>
+<link rel="shortcut icon" href="http://www.weareamber.com/wp-content/uploads/2012/10/favicon.png" type="image/x-icon" />
+<link rel="apple-touch-icon" href="http://www.weareamber.com/wp-content/uploads/2012/10/apple-touch-icon.png" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /><link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.weareamber.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.weareamber.com/wp-includes/wlwmanifest.xml" /> 
+<link rel='prev' title='Previous Post' href='http://www.weareamber.com/' />
+<meta name="generator" content="WordPress 3.4.2" />
+<link rel='canonical' href='http://www.weareamber.com/?page_id=39' />
+
+<!-- NavBar | Section Head -->
+			<!--[if IE 8]>
+				<style>
+					.nav-collapse.collapse {
+						height: auto;
+						overflow: visible;
+					}
+				</style>
+			<![endif]-->		
+		
+</head>
+
+<!-- Start >> HTML Body -->
+<body class="page page-id-39 page-template-default custom responsive spitzer default full_width  spitzer-pat0 spitzer-yellow">
+<div id="site" class="one-sidebar-right">
+	<div id="page" class="thepage">
+				<div class="page-canvas">
+						<header id="header" class="container-group">
+				<div class="outline">
+					
+
+<!-- Branding | Section Template -->
+<section id="branding" class="container no_clone section-branding fix"><div class="texture"><div class="content"><div class="content-pad"><div class="branding_wrap fix"><a class="plbrand mainlogo-link" href="http://www.weareamber.com" title="We Are AMBER"><img class="mainlogo-img" src="http://www.weareamber.com/wp-content/uploads/2012/10/amber-header.png" alt="We Are AMBER" /></a><div class="icons" style="bottom: 12px; right: 1px;"><a target="_blank" href="http://www.weareamber.com/?feed=rss2" class="rsslink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/rss.png" alt="RSS"/></a><a target="_blank" href="https://twitter.com/AmberOps" class="twitterlink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/twitter.png" alt="Twitter"/></a><a target="_blank" href="http://www.youtube.com/themalthusianparadox" class="youtubelink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/youtube.png" alt="Youtube"/></a></div></div>		
+			<script type="text/javascript"> 
+				jQuery('.icons a').hover(function(){ jQuery(this).fadeTo('fast', 1); },function(){ jQuery(this).fadeTo('fast', 0.5);});
+			</script>
+</div></div></div></section>
+
+<!-- NavBar | Section Template -->
+<section id="navbar" class="container no_clone section-navbar fix"><div class="content"><div class="content-pad">	<div class="navbar fix navbar-content-width  pl-color-black-trans">
+	  <div class="navbar-inner ">
+	    <div class="navbar-content-pad fix">
+	    		
+	      <a href="javascript:void(0)" class="nav-btn nav-btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </a>
+			
+	      		<div class="nav-collapse collapse">
+	       <ul id="menu-main" class="font-sub navline pldrop pull-left"><li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.weareamber.com/">Previous Post</a></li>
+<li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item menu-item-48"><a href="register_terms.php">Join Us</a></li>
+</ul>				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</div></div></section>				</div>
+			</header>
+						<div id="page-main" class="container-group">
+				<div id="dynamic-content" class="outline">	
+
+
+<!-- Content | Section Template -->
+<section id="content" class="container no_clone section-content-area fix"><div class="texture"><div class="content"><div class="content-pad">		<div id="pagelines_content" class="one-sidebar-right fix">
+
+						<div id="column-wrap" class="fix">
+
+								<div id="column-main" class="mcolumn fix">
+					<div class="mcolumn-pad" >
+						
+
+<!-- PostLoop | Section Template -->
+<section id="postloop" class="copy no_clone section-postloop"><div class="copy-pad"><article class="post-39 page type-page status-publish hentry fpost post-number-1" id="post-39"><div class="hentry-pad"><section class="post-meta fix post-nothumb  media"><section class="bd post-header fix" ><section class="bd post-title-section fix"><hgroup class="post-title fix">
+  <h1 class="entry-title pagetitle">Confirm Registration</h1>
+</hgroup></section> </section></section><div class="entry_wrap fix"><div class="entry_content">
+
+<!-- Begin | MRL registration code  -->
+
 <?PHP
 require_once("./include/registration.php");
 
@@ -30,5 +128,50 @@ Confirmation Code
 
 </form>
 
+<!-- End | MRL registration code  -->
+
+
+</div></div></div></article><div class="clear"></div></div></section>					</div>
+				</div>
+
+							</div>	
+			
+		<div id="sidebar-wrap" class="">
+					<div id="sidebar1" class="scolumn" >
+					<div class="scolumn-pad">
+						
+
+<!-- Primary Sidebar | Section Template -->
+<section id="sb_primary" class="copy no_clone section-sb_primary"><div class="copy-pad"><ul id="list_sb_primary" class="sidebar_widgets fix"><li id="categories-2" class="widget_categories widget fix"><div class="widget-pad"><h3 class="widget-title">Information</h3>		<ul>
+	<li class="cat-item cat-item-7"><a href="http://www.weareamber.com/?cat=7" title="View all posts filed under News">News</a>
+</li>
+		</ul>
+</div></li></ul><div class="clear"></div></div></section>	
+					</div>
+				</div>
+					</div>		
+			</div>
+</div></div></div></section>				</div>
+								<div id="morefoot_area" class="container-group">
+									</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="footer" class="container-group">
+		<div class="outline fix">
+		
+
+<!-- Simple Nav | Section Template -->
+<section id="simple_nav" class="container no_clone section-simple_nav fix"><div class="texture"><div class="content"><div class="content-pad"><div class="menu-main-container"><ul id="menu-main-1" class="inline-list simplenav font-sub"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.weareamber.com/">Previous Post</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item menu-item-48"><a href="http://www.weareamber.com/?page_id=39">Join Us</a></li>
+</ul></div></div></div></div></section><div id="cred" class="pagelines" style="display: block; visibility: visible;"><a class="plimage" target="_blank" href="http://malthusianparadox.com" title="AMBER is part of The Malthusian Paradox a transmedia ARG narrative"><img src="http://www.weareamber.com/wp-content/uploads/2012/10/AMBER-LOGO1.png" alt="AMBER is part of The Malthusian Paradox a transmedia ARG narrative" /></a></div><div class="clear"></div>		</div>
+	</footer>
+</div>
+
+<!-- Footer Scripts -->
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/js/script.bootstrap.min.js?ver=2.0.3'></script>
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/js/script.blocks.js?ver=1.0.1'></script>
 </body>
 </html>
--- register_consent.php
+++ register_consent.php
@@ -1,3 +1,121 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<!-- Title -->
+<title>We Are AMBER | Join Us</title>
+
+<!-- Start >> Meta Tags and Inline Scripts -->
+<meta name='robots' content='noindex,nofollow' />
+<link rel="alternate" type="application/rss+xml" title="We Are AMBER &raquo; Feed" href="http://www.weareamber.com/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="We Are AMBER &raquo; Comments Feed" href="http://www.weareamber.com/?feed=comments-rss2" />
+
+<!-- Styles -->
+<link rel='stylesheet' id='pagelines-less-css'  href='http://www.weareamber.com/?pageless=1_1349824153' type='text/css' media='all' />
+<link rel='stylesheet' id='pagelines-child-stylesheet-css'  href='http://www.weareamber.com/wp-content/themes/spitzer/style.css?ver=232-100292712' type='text/css' media='all' />
+
+<!-- Scripts -->
+<script type='text/javascript' src='http://www.weareamber.com/wp-includes/js/jquery/jquery.js?ver=1.7.2'></script>
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/sections/navbar/navbar.js?ver=3.4.2'></script>
+<link rel="shortcut icon" href="http://www.weareamber.com/wp-content/uploads/2012/10/favicon.png" type="image/x-icon" />
+<link rel="apple-touch-icon" href="http://www.weareamber.com/wp-content/uploads/2012/10/apple-touch-icon.png" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /><link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.weareamber.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.weareamber.com/wp-includes/wlwmanifest.xml" /> 
+<link rel='prev' title='Previous Post' href='http://www.weareamber.com/' />
+<meta name="generator" content="WordPress 3.4.2" />
+<link rel='canonical' href='http://www.weareamber.com/?page_id=39' />
+
+<!-- NavBar | Section Head -->
+			<!--[if IE 8]>
+				<style>
+					.nav-collapse.collapse {
+						height: auto;
+						overflow: visible;
+					}
+				</style>
+			<![endif]-->		
+		
+</head>
+
+<!-- Start >> HTML Body -->
+<body class="page page-id-39 page-template-default custom responsive spitzer default full_width  spitzer-pat0 spitzer-yellow">
+<div id="site" class="one-sidebar-right">
+	<div id="page" class="thepage">
+				<div class="page-canvas">
+						<header id="header" class="container-group">
+				<div class="outline">
+					
+
+<!-- Branding | Section Template -->
+<section id="branding" class="container no_clone section-branding fix"><div class="texture"><div class="content"><div class="content-pad"><div class="branding_wrap fix"><a class="plbrand mainlogo-link" href="http://www.weareamber.com" title="We Are AMBER"><img class="mainlogo-img" src="http://www.weareamber.com/wp-content/uploads/2012/10/amber-header.png" alt="We Are AMBER" /></a><div class="icons" style="bottom: 12px; right: 1px;"><a target="_blank" href="http://www.weareamber.com/?feed=rss2" class="rsslink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/rss.png" alt="RSS"/></a><a target="_blank" href="https://twitter.com/AmberOps" class="twitterlink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/twitter.png" alt="Twitter"/></a><a target="_blank" href="http://www.youtube.com/themalthusianparadox" class="youtubelink"><img src="http://www.weareamber.com/wp-content/themes/pagelines/sections/branding/youtube.png" alt="Youtube"/></a></div></div>		
+			<script type="text/javascript"> 
+				jQuery('.icons a').hover(function(){ jQuery(this).fadeTo('fast', 1); },function(){ jQuery(this).fadeTo('fast', 0.5);});
+			</script>
+</div></div></div></section>
+
+<!-- NavBar | Section Template -->
+<section id="navbar" class="container no_clone section-navbar fix"><div class="content"><div class="content-pad">	<div class="navbar fix navbar-content-width  pl-color-black-trans">
+	  <div class="navbar-inner ">
+	    <div class="navbar-content-pad fix">
+	    		
+	      <a href="javascript:void(0)" class="nav-btn nav-btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </a>
+			
+	      		<div class="nav-collapse collapse">
+	       <ul id="menu-main" class="font-sub navline pldrop pull-left"><li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.weareamber.com/">Previous Post</a></li>
+<li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item menu-item-48"><a href="register_terms.php">Join Us</a></li>
+</ul>				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</div></div></section>				</div>
+			</header>
+						<div id="page-main" class="container-group">
+				<div id="dynamic-content" class="outline">	
+
+
+<!-- Content | Section Template -->
+<section id="content" class="container no_clone section-content-area fix"><div class="texture"><div class="content"><div class="content-pad">		<div id="pagelines_content" class="one-sidebar-right fix">
+
+						<div id="column-wrap" class="fix">
+
+								<div id="column-main" class="mcolumn fix">
+					<div class="mcolumn-pad" >
+						
+
+<!-- PostLoop | Section Template -->
+<section id="postloop" class="copy no_clone section-postloop"><div class="copy-pad"><article class="post-39 page type-page status-publish hentry fpost post-number-1" id="post-39"><div class="hentry-pad"><section class="post-meta fix post-nothumb  media"><section class="bd post-header fix" ><section class="bd post-title-section fix"><hgroup class="post-title fix">
+  <h1 class="entry-title pagetitle">The Malthusian Paradox Study</h1>
+</hgroup></section> </section></section><div class="entry_wrap fix"><div class="entry_content">
+
+<!-- Begin | MRL registration code  -->
+
+<?php
+
+session_start();
+
+if(isset($_POST["terms"]))
+{
+	$_SESSION['terms'] = true;
+}
+else
+{
+	$_SESSION['terms'] = false;
+}
+
+$_SESSION['player_info'] = array();
+$_SESSION['player_info']['demographics'] = array();
+
+$_SESSION['player_info']['demographics']['1'] = date($_POST['day'] . "/" . $_POST['month'] . "/" . $_POST['year']);
+
+?>
+
 <?php
 
 session_start();
@@ -106,10 +224,6 @@ else
 ?>
 
 <html>
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<body>
-
-<h1>The Malthusian Paradox Study</h1>
 
 <p>
   <label for="study"></label>
@@ -168,22 +282,17 @@ We need your consent to allow us to collect and analyse data regarding your part
 </p>
 <p>&nbsp; </p>
 <p>
-  <input type="checkbox" name="1" value="true"> I have read and understand the attached information sheet, which includes information about the research project, and the data that may be recorded and published. I understand that I can withdraw consent for my participation at any time, and my personal data will be erased from our records and systems.</p>
-<p>&nbsp;</p>
-
+  <input name="1" type="checkbox" value="true"> I have read and understand the attached information sheet, which includes information about the research project, and the data that may be recorded and published. I understand that I can withdraw consent for my participation at any time, and my personal data will be erased from our records and systems.</p>
 <p>
-<input type="checkbox" name="2" value="true"> I give permission for digital records generated during the course of my interaction with the Malthusian Paradox, including information I submit to the game’s websites, to be used as part of the study in an anonymised form.
+  <input type="checkbox" name="2" value="true">
+ I give permission for digital records generated during the course of my interaction with The Malthusian Paradox, including information I submit to the  websites, to be used as part of the study in an anonymised form.
 </p>
-<p>&nbsp;</p>
-
 <p>
-<input type="checkbox" name="3" value="true"> I give consent for video and audio recordings of my interactions with the game at participating venues to be collected, and I agree to the use of this data in an anonymised form.
+  <input type="checkbox" name="3" value="true"> I give consent for video and audio recordings of my interactions with the game at participating venues to be collected, and I agree to the use of this data in an anonymised form.
 </p>
-<p>&nbsp;</p>
-
 <p>
-<input type="checkbox" name="4" value="true"> I give consent to taking part in a structured interview regarding my experience of the Malthusian Paradox at the conclusion of the game, and I agree to the use of this data in an anonymised form.
-</p>
+  <input type="checkbox" name="4" value="true"> 
+I give consent to taking part in a structured interview regarding my experience of The Malthusian Paradox at the conclusion of the game, and I agree to the use of this data in an anonymised form.</p>
 <p>&nbsp;</p>
 
 <p>
@@ -192,5 +301,49 @@ We need your consent to allow us to collect and analyse data regarding your part
 
 </form>
 
+<!-- End | MRL registration code  -->
+
+</div></div></div></article><div class="clear"></div></div></section>					</div>
+				</div>
+
+							</div>	
+			
+		<div id="sidebar-wrap" class="">
+					<div id="sidebar1" class="scolumn" >
+					<div class="scolumn-pad">
+						
+
+<!-- Primary Sidebar | Section Template -->
+<section id="sb_primary" class="copy no_clone section-sb_primary"><div class="copy-pad"><ul id="list_sb_primary" class="sidebar_widgets fix"><li id="categories-2" class="widget_categories widget fix"><div class="widget-pad"><h3 class="widget-title">Information</h3>		<ul>
+	<li class="cat-item cat-item-7"><a href="http://www.weareamber.com/?cat=7" title="View all posts filed under News">News</a>
+</li>
+		</ul>
+</div></li></ul><div class="clear"></div></div></section>	
+					</div>
+				</div>
+					</div>		
+			</div>
+</div></div></div></section>				</div>
+								<div id="morefoot_area" class="container-group">
+									</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="footer" class="container-group">
+		<div class="outline fix">
+		
+
+<!-- Simple Nav | Section Template -->
+<section id="simple_nav" class="container no_clone section-simple_nav fix"><div class="texture"><div class="content"><div class="content-pad"><div class="menu-main-container"><ul id="menu-main-1" class="inline-list simplenav font-sub"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.weareamber.com/">Previous Post</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item menu-item-48"><a href="http://www.weareamber.com/?page_id=39">Join Us</a></li>
+</ul></div></div></div></div></section><div id="cred" class="pagelines" style="display: block; visibility: visible;"><a class="plimage" target="_blank" href="http://malthusianparadox.com" title="AMBER is part of The Malthusian Paradox a transmedia ARG narrative"><img src="http://www.weareamber.com/wp-content/uploads/2012/10/AMBER-LOGO1.png" alt="AMBER is part of The Malthusian Paradox a transmedia ARG narrative" /></a></div><div class="clear"></div>		</div>
+	</footer>
+</div>
+
+<!-- Footer Scripts -->
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/js/script.bootstrap.min.js?ver=2.0.3'></script>
+<script type='text/javascript' src='http://www.weareamber.com/wp-content/themes/pagelines/js/script.blocks.js?ver=1.0.1'></script>
 </body>
 </html>
